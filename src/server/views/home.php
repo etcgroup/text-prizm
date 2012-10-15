16df4607d6baad5837d9bf384db96720458255b9
Michael Brooks <mjbrooks@uw.edu>
2012-10-15 00:12:08 -0700

    excuse poor home.php view

--- src/server/views/home.php
+++ src/server/views/home.php
@@ -3,11 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>Home</title>
-        <?php echo css('reset.css'); ?>
     </head>
     <body>
         <h1>Hello <?php echo $user->name ?>!</h1>
-        <p>Some useful links:</p>
+        <p>This is a not-very-interesting placeholder page. Here are some useful links:</p>
         <ul>
             <li><a href="codingstats">Coding Stats</a></li>
             <li><a href="coding">Coding Tools</a></li>
