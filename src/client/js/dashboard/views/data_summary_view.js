906044e2a03d8b388c097c16f3146ec8c1cd1f66
Michael Brooks <mjbrooks@uw.edu>
2012-11-04 15:38:50 -0800

    add data_summary view template and css
    issue #42

--- src/client/js/dashboard/views/data_summary_view.js
+++ src/client/js/dashboard/views/data_summary_view.js
@@ -9,8 +9,18 @@ define(['marionette',
         var DatSetSummaryView = Marionette.ItemView.extend({
             template: dataSummaryTemplate,
             initialize: function() {
-                this.model = new DataCounts();
-                this.model.fetch();
+                //Temporarily load the model with fake data
+                this.model = new DataCounts({
+                    messages: 356134,
+                    participants: 673,
+                    codes: 46,
+                    total_memos: 32,
+                    recent_memos: 4,
+                    clusters: 1002,
+                    coded_messages: 23015,
+                    active_coders: 3
+                });
+                //this.model.fetch();
             }
         });
 
