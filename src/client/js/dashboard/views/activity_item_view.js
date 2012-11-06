8fcb82f05b265b92d057fd0dd17d2d7a73f4d518
Michael Brooks <mjbrooks@uw.edu>
2012-11-05 20:15:39 -0800

    fill in activities view, template, and sub-templates

--- src/client/js/dashboard/views/activity_item_view.js
+++ src/client/js/dashboard/views/activity_item_view.js
@@ -1,12 +1,35 @@
-define(['marionette',
-    'text!../templates/activity_item.html'],
-    function(Marionette, activityItemTemplate) {
+define(['jquery', 'marionette', 'underscore', 'backbone',
+    'common/helpers',
+    'common/models/activity',
+    'text!../templates/activity_item.html',
+    'text!../templates/activities.html'],
+    function($, Marionette, _, Backbone,
+        Helpers, Activity, activityItemTemplate, activitiesTemplates) {
+
+        var activity_templates = {};
+
+        activitiesTemplates = $(activitiesTemplates).children();
+        activitiesTemplates.each(function(idx, element) {
+            activity_templates[this.id] = _.template($(this).html());
+        });
+
         /**
          * A view that summarizes an item of recent activity.
          */
         var ActivityItemView = Marionette.ItemView.extend({
             template: activityItemTemplate,
-            tagName: 'li'
+            templateHelpers: Helpers,
+            tagName: 'li',
+            className: 'activity',
+
+            serializeData: function() {
+                var data = this.model.toJSON();
+
+                //Insert the correct activity template for this activity type
+                data.activity_body = activity_templates[this.model.get('type')];
+
+                return data;
+            }
         });
 
         return ActivityItemView;
