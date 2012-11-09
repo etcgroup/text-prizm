602ca788575f980a1c28df89c4362ead4724c229
Michael Brooks <mjbrooks@uw.edu>
2012-11-09 14:40:42 -0800

    add new fields to config/app.php
    Fields are filled in during installation in manage.php
    These are based on values in the install config file (.install.ini)

--- src/server/config/app.php
+++ src/server/config/app.php
@@ -7,4 +7,7 @@ $config['name'] = 'My App';
 $config['version'] = '';
 $config['build_time'] = '';
 $config['revision'] = '';
+$config['upgrade_time'] = '';
+$config['database_hostname'] = '';
+$config['database_schema'] = '';
 
