af7efae8e2f2ba9d3b2c32b773c4fbb1a6684d43
Michael Brooks <mjbrooks@uw.edu>
2012-11-17 15:05:02 -0800

    add proper url and defaults for data_counts.js model

--- src/client/js/common/models/data_counts.js
+++ src/client/js/common/models/data_counts.js
@@ -3,7 +3,21 @@ define(['backbone'], function(Backbone) {
      * Model for storing counts info about the data set.
      */
     var DataCounts = Backbone.Model.extend({
-        url: '/ext/datacounts'
+        url: 'ext/data_counts/summary',
+
+        defaults: {
+            messages: 0,
+            participants: 0,
+            codes: 0,
+            clusters: 0,
+            coded_messages: 0,
+            coded_messages_recent: 0,
+            coders: 0,
+            coders_recent: 0,
+            memos: 0,
+            memos_recent: 0,
+            recency: 0
+        }
     });
     return DataCounts;
 });
