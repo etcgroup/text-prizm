ad792728b822773d8770692bdff8f09aacc8e535
Michael Brooks <mjbrooks@uw.edu>
2012-11-17 16:29:05 -0800

    data summary view uses spinner

--- src/client/js/dashboard/views/data_summary_view.js
+++ src/client/js/dashboard/views/data_summary_view.js
@@ -1,7 +1,7 @@
-define(['marionette',
+define(['marionette', 'lib/spin',
     'text!../templates/data_summary.html',
     'common/helpers/all'],
-    function(Marionette, dataSummaryTemplate, Helpers) {
+    function(Marionette, Spinner, dataSummaryTemplate, Helpers) {
 
         /**
          * A view summarizing information about the data set.
@@ -10,7 +10,48 @@ define(['marionette',
             template: dataSummaryTemplate,
             templateHelpers: Helpers,
             modelEvents: {
-                'change': 'render'
+                'change': 'modelChanged'
+            },
+
+            modelChanged: function() {
+                if (this.model.get('loaded')) {
+                    this.disableSpinner();
+                }
+
+                this.render();
+            },
+
+            onRender: function(){
+                if (!this.model.get('loaded')) {
+                    this.enableSpinner();
+                }
+            },
+
+            /**
+             * Enables the spinner.
+             */
+            enableSpinner: function() {
+                if (!this.spinner) {
+                    this.spinner = new Spinner();
+                }
+                var target = $('<div class="spinner-box">');
+                this.$el.find('.row-fluid').append(target);
+
+                this.spinner.spin(target[0]);
+                $(this.spinner.el).css({
+                    top: '50%',
+                    left: '50%'
+                });
+            },
+
+            /**
+             * Stops the spinner if it has been enabled.
+             */
+            disableSpinner: function() {
+                if (this.spinner) {
+                    this.spinner.stop();
+                    this.$el.find('.spinner-box').remove();
+                }
             }
         });
 
