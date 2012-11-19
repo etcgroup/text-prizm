a23106b1692b427bd6faf199f8849cd7eb5c9abe
Michael Brooks <mjbrooks@uw.edu>
2012-11-19 10:30:28 -0800

    add tests for bootstrap helper

---
+++ tests/client/js/common/helpers/bootstrap_spec.js
@@ -0,0 +1,20 @@
+define(['common/helpers/bootstrap'], function(BootstrapHelper) {
+    describe("BootstrapHelper", function() {
+
+        it("can render a normal icon", function() {
+            var icon_name = 'my-icon';
+            var icon = $(BootstrapHelper.icon(icon_name));
+
+            expect(icon).toBe('i.icon-' + icon_name);
+            expect(icon).not.toBe('i.icon-white');
+        });
+
+        it("can render a white icon", function() {
+            var icon_name = 'my-icon';
+            var icon = $(BootstrapHelper.icon_white(icon_name));
+
+            expect(icon).toBe('i.icon-' + icon_name);
+            expect(icon).toBe('i.icon-white');
+        });
+    });
+});
