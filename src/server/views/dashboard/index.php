8730c1418fb47fa9a475076ffb171ab383c6b8a8
Michael Brooks <mjbrooks@uw.edu>
2012-11-20 11:56:06 -0800

    move app status back to left

--- src/server/views/dashboard/index.php
+++ src/server/views/dashboard/index.php
@@ -1,10 +1,10 @@
 <div id="dashboard" class="row-fluid">
     <div class="span6">
         <div id="data-set-summary-widget" class="panel"></div>
+        <div id="app-status-widget" class="panel"></div>
     </div>
     <div class="span6">
         <div id="activity-list" class="panel"></div>
-        <div id="app-status-widget" class="panel"></div>
     </div>
 
 </div>
