98315be460564a1c594486437851d4902942d394
Katie Kuksenok <katie.kuksenok@gmail.com>
2012-12-02 20:17:04 -0800

    Add intermediate model and feature recording relations

--- src/server/migrations/018_add_features.php
+++ src/server/migrations/018_add_features.php
@@ -3,37 +3,49 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Creates the 'didi_machines' table.
- * Fields: id, location, last_ping, is_busy
- * location contains ip:port and name contains some human-readable name
  */
-class Migration_Add_didi_abilities extends CI_Migration {
+class Migration_Add_features extends CI_Migration {
 
     public function up()
     {
-
         //Create the fields
         $this->dbforge->add_field(array(
-            'machine_id' => array(
+            'id' => array(
+                'type' => 'INT',
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'didi_task_id' => array(
                 'type' => 'INT',
                 'unsigned' => TRUE
             ),
-            'task_type' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 200
+            'message_id' => array(
+                'type' => 'INT',
+                'unsigned' => TRUE
+            ),
+            'name' => array(
+                'type' => 'INT',
+                'unsigned' => TRUE
+            ),
+            'value' => array(
+                'type' => 'TEXT'
             )
         ));
 
         // Add the primary key
-        $this->dbforge->add_key(array('machine_id', 'task_type'), TRUE);
+        $this->dbforge->add_key('id', TRUE);
+        
+        // add more indeces
+        $this->dbforge->add_key('name');
+        $this->dbforge->add_key('didi_task_id');
         
         //Create the table
-        $this->dbforge->create_table('didi_abilities');
+        $this->dbforge->create_table('features');
     }
 
     public function down()
     {
-        $this->dbforge->drop_table('didi_abilities');
+        $this->dbforge->drop_table('features');
     }
 
 }
