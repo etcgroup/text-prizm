f9c4e03c4a62fa52ebe559c463bc182a95491f54
Michael Brooks <mjbrooks@uw.edu>
2012-12-02 16:48:29 -0800

    fill in job item views

--- src/client/js/didiera/views/job_item_view.js
+++ src/client/js/didiera/views/job_item_view.js
@@ -1,13 +1,16 @@
-define(['marionette'],
-    function(Marionette) {
+define(['marionette',
+    'common/helpers/all',
+    'text!../templates/job_item.html'],
+    function(Marionette, Helpers, jobItemTemplate) {
 
         /**
          * A view that summarizes a job.
          */
         var JobItemView = Marionette.ItemView.extend({
-            template: "I'm a job",
+            template: jobItemTemplate,
             tagName: 'li',
-            className: 'job'
+            className: 'job',
+            templateHelpers: Helpers
         });
 
         return JobItemView;
