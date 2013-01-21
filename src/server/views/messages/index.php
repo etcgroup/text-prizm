2856d36abe6f8e0d2bf24fae9faa90c863b246ca
Michael Brooks <mjbrooks@uw.edu>
2013-01-20 17:30:37 -0800

    add messages controller server side
    with a special route for html5 pushstate stuff

---
+++ src/server/views/messages/index.php
@@ -0,0 +1,10 @@
+<div id="messages" class="row-fluid">
+    <div class="span12">
+        <div class="row-fluid">
+            <div id="message-navigator"></div>
+        </div>
+        <div class="row-fluid">
+            <div id="message-viewer"></div>
+        </div>
+    </div>
+</div>
