aac2fe20573c3596d1260ed3339d15b0e007e5c2
Michael Brooks <mjbrooks@uw.edu>
2013-01-25 22:41:26 -0800

    message list, item, applied code views

--- src/client/js/messages/views/message_item_view.js
+++ src/client/js/messages/views/message_item_view.js
@@ -1,18 +1,33 @@
 define(['marionette', 'underscore',
     'common/helpers/all',
+    '../views/applied_code_view',
     'text!../templates/message_item.html'],
     function(Marionette, _,
-        Helpers, messageItemTemplate) {
-
+        Helpers, AppliedCodeView, messageItemTemplate) {
 
         /**
          * A view that displays a single message.
          */
-        var MessageItemView = Marionette.ItemView.extend({
+        var MessageItemView = Marionette.CompositeView.extend({
             template: messageItemTemplate,
             templateHelpers: Helpers,
+
+            itemView: AppliedCodeView,
+            itemViewContainer: 'ul.instances',
+
             tagName: 'li',
-            className: 'message-item'
+            className: 'message-item',
+
+            ui: {
+                timeLink: '.time-link',
+                appliedCodes: '.applied-code'
+            },
+
+            onRender: function() {
+                this.ui.timeLink.tooltip({
+                    placement: 'right'
+                });
+            }
         });
 
         return MessageItemView;
--- src/client/js/messages/views/message_list_view.js
+++ src/client/js/messages/views/message_list_view.js
@@ -1,7 +1,7 @@
-define(['marionette',
+define(['backbone', 'marionette',
     '../views/message_item_view',
     'text!../templates/message_list.html'],
-    function(Marionette, MessageItemView, messageListTemplate) {
+    function(Backbone, Marionette, MessageItemView, messageListTemplate) {
 
         /**
          * A view shown when no messages are loaded.
@@ -18,7 +18,68 @@ define(['marionette',
             template: messageListTemplate,
             itemView: MessageItemView,
             emptyView: EmptyMessageListView,
-            itemViewContainer: 'ul.message-list'
+            itemViewContainer: 'ul.message-list',
+            className: 'message-list-view',
+
+            ui: {
+                messageList: 'ul.message-list'
+            },
+
+            events: {
+                'scroll': 'loadMoreMessages'
+            },
+
+            collectionEvents: {
+                'batch-add': 'renderMoreMessages'
+            },
+
+            // Build an `itemView` for every model in the collection.
+            buildItemView: function(item, ItemViewType, itemViewOptions){
+
+                if (ItemViewType !== MessageItemView) {
+                    return Marionette.CompositeView.prototype.buildItemView.apply(this, arguments);
+                } else {
+                    //group the code instances
+                    var instancesByCodeId = item.get('instances').groupBy(function(instance) {
+                        return instance.get('code').get('id');
+                    });
+
+                    var appliedCodes = new Backbone.Collection();
+                    _.each(instancesByCodeId, function(instancesForCode) {
+                        var codeModel = instancesForCode[0].get('code');
+                        appliedCodes.add(new Backbone.Model({
+                            code: codeModel,
+                            instances: new Backbone.Collection(instancesForCode)
+                        }));
+                    });
+
+                    var options = _.extend({
+                        model: item,
+                        collection: appliedCodes
+                    }, itemViewOptions);
+                    var view = new ItemViewType(options);
+                    return view;
+                }
+            },
+
+            loadMoreMessages: function() {
+                var totalHeight = this.ui.messageList.height();
+                var scrollTop = this.$el.scrollTop() + this.$el.height();
+                var margin = 200;
+
+                if (scrollTop + margin >= totalHeight) {
+                    this.collection.fetchMoreMessages();
+                }
+            },
+
+            renderMoreMessages: function(options) {
+                var ItemView = this.getItemView();
+
+                for (var i = options.from; i < options.to; i++) {
+                    var item = this.collection.at(i);
+                    this.addItemView(item, ItemView, i);
+                }
+            }
         });
 
         return MessageListView;
