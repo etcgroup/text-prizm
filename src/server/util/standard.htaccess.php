49f191bb7278a3a49e20c137162fbf46d377e8f3
Michael Brooks <mjbrooks@uw.edu>
2013-01-26 02:34:20 -0800

    redirect slash-terminated urls to not

--- src/server/util/standard.htaccess.php
+++ src/server/util/standard.htaccess.php
@@ -3,5 +3,9 @@
 RewriteEngine on
 RewriteBase <?php echo $this->install_config['site_path']; ?>
 
+# Rewrite trailing slashes to non-trailing-slash equivalents
+RewriteCond %{REQUEST_URI} /$ [NC]
+RewriteRule ^(.*)/$ $1 [NC,L,R=301]
+
 RewriteCond $1 !^(index\.php|assets|robots\.txt)
 RewriteRule ^(.*)$ index.php?/$1 [L]
