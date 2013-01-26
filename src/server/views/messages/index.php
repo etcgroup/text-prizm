c6e0d15296771cf8c05ee9e5cd9042f832526a60
Michael Brooks <mjbrooks@uw.edu>
2013-01-25 22:40:05 -0800

    adjust messages layout

--- src/server/views/messages/index.php
+++ src/server/views/messages/index.php
@@ -1,10 +1,6 @@
 <div id="messages" class="row-fluid">
     <div class="span12">
-        <div class="row-fluid">
-            <div id="message-navigator"></div>
-        </div>
-        <div class="row-fluid">
-            <div id="message-viewer"></div>
-        </div>
+        <div id="message-navigator"></div>
+        <div id="message-viewer"></div>
     </div>
 </div>
