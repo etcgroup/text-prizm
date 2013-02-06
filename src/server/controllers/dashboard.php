538a860c6248738e6dbf795690be030788372909
Michael Brooks <mjbrooks@uw.edu>
2013-02-06 11:59:10 -0800

    check for bad dashboard urls

--- src/server/controllers/dashboard.php
+++ src/server/controllers/dashboard.php
@@ -7,6 +7,12 @@ class Dashboard extends Base_Controller {
 
     public function index()
     {
+        $current_url = current_url();
+        $ends_with_dashboard = strlen($current_url) - strrpos($current_url, 'dashboard') === strlen('dashboard');
+        if ($ends_with_dashboard) {
+            redirect('/', 'location');
+        }
+
         $user = $this->ensure_user();
 
         $data = array(
